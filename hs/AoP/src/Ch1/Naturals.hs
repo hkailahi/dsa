4ce1b64d2411a8121c9bc55c82742df8ed987a50
Heneli Kailahi <heneli@kailahi.com>
2019-07-28 14:37:13 -0400

    blah

--- hs/AoP/src/Ch1/Naturals.hs
+++ hs/AoP/src/Ch1/Naturals.hs
@@ -1,6 +1,7 @@
 module Ch1.Naturals where
 
-import Prelude (Eq, Show, Ord, ($), Int, (+), (-), (>=), (==), (*), error, take, repeat, (.), foldr)
+import qualified Prelude as P
+import Prelude (Eq, Show, Ord, ($), Int, (-), (>=), (==), error, take, repeat, (.), foldr)
 
 data Nat =
     Zero
@@ -22,7 +23,7 @@ fromNat = helper 0
   where
     helper :: Int -> Nat -> Int
     helper n Zero       = n
-    helper n (Succ nat) = helper (n + 1) nat
+    helper n (Succ nat) = helper (n P.+ 1) nat
 
 -- plus : Nat <- (Nat x Nat)
 plus :: (Nat, Nat) -> Nat
@@ -34,12 +35,18 @@ mult :: (Nat, Nat) -> Nat
 mult (m, Zero)   = Zero
 mult (m, Succ n) = plus (m, mult (m, n))
 
+(+) = P.curry plus
+infixl 6 +
+
+(*) = P.curry mult
+infixl 7 *
+
 shittyFact :: Int -> Int
 shittyFact = \case
   0 -> 1
   -- behold shitty n+k patterns
   n | n >= 1 -> let k = n - 1
-            in  (k + 1) * shittyFact k
+            in  (k P.+ 1) P.* shittyFact k
   otherwise -> error "lol"
 
 shittyFib :: Int -> Int
@@ -48,18 +55,18 @@ shittyFib = \case
   1 -> 1
   -- behold shitty n+k patterns
   n | n >= 2 -> let k = n - 2
-                 in shittyFib k + shittyFib (k + 1)
+                 in shittyFib k P.+ shittyFib (k P.+ 1)
   otherwise  -> error "lol"
 
 -- fact : Nat <- Nat
 natFact :: Nat -> Nat
 natFact = \case
   Zero   -> One
-  Succ n -> mult (plus (n, One), natFact n)
+  Succ n -> (n + One) * natFact n
 
 -- fib : Nat <- Nat
 natFib :: Nat -> Nat
 natFib = \case
   Zero          -> Zero
   One           -> One
-  Succ (Succ n) -> plus (natFib n, natFib $ plus (n, One))
+  Succ (Succ n) -> natFib n + natFib (n + One)
