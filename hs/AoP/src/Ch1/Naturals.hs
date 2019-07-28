d65393f125765f6c63c2d3b1ce351859c61c0a0a
Heneli Kailahi <heneli@kailahi.com>
2019-07-28 14:26:45 -0400

    more basic shit

--- hs/AoP/src/Ch1/Naturals.hs
+++ hs/AoP/src/Ch1/Naturals.hs
@@ -1,6 +1,6 @@
 module Ch1.Naturals where
 
-import Prelude (Eq, Show, Ord, ($), Int, (+), (-), (>=), (==), (*), error)
+import Prelude (Eq, Show, Ord, ($), Int, (+), (-), (>=), (==), (*), error, take, repeat, (.), foldr)
 
 data Nat =
     Zero
@@ -8,6 +8,21 @@ data Nat =
   deriving (Show, Eq, Ord)
 zero = Zero
 succ = Succ
+pattern One :: Nat
+pattern One = Succ Zero
+
+toNat :: Int -> Nat
+toNat n =
+  foldr (\f b -> f b) Zero
+    . take n
+    $ repeat Succ
+
+fromNat :: Nat -> Int
+fromNat = helper 0
+  where
+    helper :: Int -> Nat -> Int
+    helper n Zero       = n
+    helper n (Succ nat) = helper (n + 1) nat
 
 -- plus : Nat <- (Nat x Nat)
 plus :: (Nat, Nat) -> Nat
@@ -19,21 +34,32 @@ mult :: (Nat, Nat) -> Nat
 mult (m, Zero)   = Zero
 mult (m, Succ n) = plus (m, mult (m, n))
 
--- fact : Nat <- Nat
-fact :: Int -> Int
-fact = \case
+shittyFact :: Int -> Int
+shittyFact = \case
   0 -> 1
   -- behold shitty n+k patterns
   n | n >= 1 -> let k = n - 1
-            in  (k + 1) * fact k
+            in  (k + 1) * shittyFact k
   otherwise -> error "lol"
 
--- fib : Nat <- Nat
-fib :: Int -> Int
-fib = \case
+shittyFib :: Int -> Int
+shittyFib = \case
   0 ->  0
   1 -> 1
   -- behold shitty n+k patterns
   n | n >= 2 -> let k = n - 2
-                 in  fib k + fib (k + 1)
+                 in shittyFib k + shittyFib (k + 1)
   otherwise  -> error "lol"
+
+-- fact : Nat <- Nat
+natFact :: Nat -> Nat
+natFact = \case
+  Zero   -> One
+  Succ n -> mult (plus (n, One), natFact n)
+
+-- fib : Nat <- Nat
+natFib :: Nat -> Nat
+natFib = \case
+  Zero          -> Zero
+  One           -> One
+  Succ (Succ n) -> plus (natFib n, natFib $ plus (n, One))
